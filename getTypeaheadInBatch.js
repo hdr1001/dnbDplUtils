90d324d25083d3d9ea7c36db90213702ad0a8747
Hans de Rooij <HansDeRooij@Hotmail.com>
2021-03-21 21:38:10 +0100

    Major upgrade of D&B Direct+ Typeahead utility

--- getTypeaheadInBatch.js
+++ getTypeaheadInBatch.js
@@ -30,10 +30,12 @@ const RateLimiter = require('limiter').RateLimiter;
 
 //Application settings
 const maxTPS = 4; //Maximum transactions per second
+const limiter = new RateLimiter(maxTPS, 'second');
 const fileCredentials = 'dnbDplCredentials.json';
 const filePathIn = {root: '', dir: 'in', base: 'MatchCriteria.txt'};
 const filePathOut = {root: '', dir: 'out'};
-const fileBase1stPt = 'typeahead_results_len30_';
+const fileBase1stPt = 'typeahead_results_';
+const delim = '|';
 const sDate = new Date().toISOString().split('T')[0];
 
 //Get a (hopefully) valid API token from file dnbDplCredentials.json
@@ -47,65 +49,159 @@ else {
    process.exit();
 }
 
-//Read & parse the typeahead criteria from the file MatchCriteria.txt
-let arrCriteria = fs.readFileSync(path.format(filePathIn)).toString().split('\n');
+const oPipeDelimPath = {...filePathOut};
+oPipeDelimPath.base = fileBase1stPt + sDate + '.txt';
+const wstream = fs.createWriteStream(path.format(oPipeDelimPath), {flags: 'a', encoding: 'utf8'});
 
-//Loop over the array of DUNS at maximum TPS
-const limiter = new RateLimiter(maxTPS, 'second');
+let workList = fs.readFileSync(path.format(filePathIn)).toString().split('\n')
+   .map(elem => elem.split('|'))
+   .map(criteria => {
 
-//The typeahead criteria as listed in the file
-const qryParams = ['searchTerm', 'countryISOAlpha2Code', 'customerReference'];
-
-arrCriteria.forEach(sCriteria => {
-   limiter.removeTokens(1, () => {
-      const oQryStr = {}, arrCriteriaSubmitted = sCriteria.split('|');
-
-      qryParams.forEach((qryParam, idx) => {
-         oQryStr[qryParam] = arrCriteriaSubmitted[idx]
-      });
-
-      console.log('About to submit ' + oQryStr.searchTerm);
-      
-      reqTypeahead(oQryStr).then(respBody => {
-         //Write the the HTTP response body to a file
-         if(filePathOut) {
-            const oFilePath = {...filePathOut};
-            oFilePath.base = fileBase1stPt + oQryStr.customerReference + '_' + sDate + '.json';
-
-            fs.writeFile(path.format(oFilePath), respBody, err => {
-               if(err) {console.log(err.message)}
-            });
+      return {
+         typeahead: {
+            searchTerm: criteria[0],
+            countryISOAlpha2Code: criteria[1],
+            customerReference: criteria[2],
+         },
+         results: {
+            done: false
          }
-      })
-      .catch(err => console.log(err))
-   })
-});
-
-//Launch an API request for D&B Direct+ Data blocks
-function reqTypeahead(oQryStr) {
-   const httpAttr = {
-      host: 'plus.dnb.com',
-      path: '/v1/search/typeahead',
-      method: 'GET',
-      headers: {
-         'Content-Type': 'application/json',
-         Authorization: 'Bearer ' + oCredentials.token
       }
-   };
-
-   httpAttr.path += '?' + qryStr.stringify(oQryStr);
-
-   return new Promise((resolve, reject) => {
-      https.request(httpAttr, resp => {
-         const body = [];
-
-         resp.on('error', err => reject(err));
-
-         resp.on('data', chunk => body.push(chunk));
-
-         resp.on('end', () => { //The match candidates now available
-            resolve(body);
-         });
-      }).end()
    });
+
+let arrRanges = [30, 20, 15, 10, 6, 3];
+
+(function processWorkList(idx) {
+   const minLen = idx < arrRanges.length - 1 ? arrRanges[idx + 1] : 0;
+   const maxLen = arrRanges[idx];
+   console.log('Range [' + minLen + ', ' + maxLen + ']');
+
+   Promise.all(
+      workList
+         .filter(workItem => workItem.typeahead.searchTerm.length > minLen && !workItem.results.done)
+         .map(workItem => {
+            const httpAttr = {
+               host: 'plus.dnb.com',
+               path: '/v1/search/typeahead',
+               method: 'GET',
+               headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: 'Bearer ' + oCredentials.token
+               }
+            };
+
+            const qryParameters = {...workItem.typeahead};
+            qryParameters.searchTerm = qryParameters.searchTerm.slice(0, maxLen);
+            httpAttr.path += '?' + qryStr.stringify(qryParameters);
+
+            return new Promise((resolve, reject) => {
+               limiter.removeTokens(1, () => {
+                  https.request(httpAttr, resp => {
+                     const body = [];
+            
+                     resp.on('error', err => reject(err));
+            
+                     resp.on('data', chunk => body.push(chunk));
+            
+                     resp.on('end', () => { //The match candidates now available
+                        workItem.results.httpResp = body;
+                        if(resp.statusCode === 200) { workItem.results.done = true }
+                        workItem.results.httpStatus = resp.statusCode;
+                        workItem.results.maxLen = maxLen;
+
+                        resolve(workItem);
+                     });
+                  }).end()
+               })
+            });
+   })).then(retWorkList => {
+               //Write the the HTTP response body to a file
+               if(filePathOut) {
+                  retWorkList.forEach(workItem => {
+                     if(workItem.results.done) {
+                        const oFilePath = {...filePathOut};
+                        oFilePath.base = fileBase1stPt + workItem.typeahead.customerReference + '_' + workItem.results.maxLen + '_' + sDate + '.json';
+   
+                        fs.writeFile(path.format(oFilePath), workItem.results.httpResp, err => {
+                           if(err) {console.log(err.message)}
+                        });
+                     }
+                  })
+               };
+
+               retWorkList
+                  .filter(workItem => workItem.results.done)
+                  .forEach(workItem => {
+                     writeTypeaheadInput(workItem.typeahead);
+
+                     const oRespTA = JSON.parse(workItem.results.httpResp);
+
+                     if(oRespTA.error || !oRespTA.searchCandidates[0]) {
+                        wstream.write(oRespTA.error.errorCode)
+                     }
+                     else {
+                        writeTypeaheadOutput(workItem.results, oRespTA)
+                     }
+                  })
+
+               if(idx < arrRanges.length - 2) { processWorkList(++idx) }
+            })
+      .catch()
+})(0);
+
+function writeTypeaheadInput(typeahead) {
+   wstream.write([typeahead.customerReference, typeahead.searchTerm, typeahead.countryISOAlpha2Code].join(delim) + delim)
+}
+
+function writeTypeaheadOutput(results, oRespTA) {
+   const arrOut = [];
+
+   arrOut.push(results.httpStatus ? results.httpStatus.toString() : '');
+
+   const candidate0 = oRespTA.searchCandidates[0];
+
+   const {duns, primaryName, tradeStyleNames, primaryAddress, dunsControlStatus} = candidate0.organization;
+
+   arrOut.push(duns);
+   arrOut.push(primaryName ? primaryName : '');
+
+   if(tradeStyleNames && tradeStyleNames.length) { arrOut.push(tradeStyleNames[0].name ? tradeStyleNames[0].name : '') }
+
+   if(primaryAddress) {
+      const {streetAddress, addressLocality, postalCode, addressCountry} = primaryAddress;
+
+      if(streetAddress && streetAddress.line1) {
+         arrOut.push(streetAddress.line1)
+      }
+      else {
+         arrOut.push('')
+      }
+
+      if(addressLocality && addressLocality.name) {
+         arrOut.push(addressLocality.name)
+      }
+      else {
+         arrOut.push('')
+      }
+
+      arrOut.push(postalCode ? postalCode : '');
+
+      if(addressCountry && addressCountry.isoAlpha2Code) {
+         arrOut.push(addressCountry.isoAlpha2Code)
+      }
+      else {
+         arrOut.push('')
+      }
+   }
+
+   if(dunsControlStatus && typeof dunsControlStatus.isOutOfBusiness === 'boolean') {
+      arrOut.push(dunsControlStatus.isOutOfBusiness)
+   }
+   else {
+      arrOut.push('')
+   }
+
+   arrOut.push(results.maxLen ? results.maxLen.toString() : '');
+
+   wstream.write(arrOut.join(delim) + '\n');
 }
